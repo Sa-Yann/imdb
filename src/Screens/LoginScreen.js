59b9124cc59f8f4afee68c2dcde7cd28b81620e4
Yann Leblond <knt4xyz@gmail.com>
2022-01-21 18:25:46 +0100

    ✅Build the jsx and css from the LoginScreen View

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -2,11 +2,47 @@ import React from 'react';
 import '../styles/loginSreenStyle.css';
 
 function LoginScreen() {
-  return (
-       <div>
-            <h1>I AM THE LOGIN SCREEN</h1>
+
+
+
+    return (
+       <div className="loginScreen">
+            {/* <h1>I AM THE LOGIN SCREEN</h1> */}
+            <div className="logScrnn_BckGrnd">
+                <img
+                    className="lgnScreen__Logo"
+                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                    alt="logo_Netflix"
+                />
+            <button 
+                className="lgnScreen__Btn"
+            >
+                Sign In
+            </button>
+            <div className="lgnScreen__Gradient" />
+            </div>
+            <div className="lgnScreen__Content">
+                <>
+                     <h1>Unlimited films, TV Programmes and more...</h1>
+                     <h2>Watch anywhere. Cancet at any time.</h2>
+                     <h3>Ready to watch? Enter your email to create or restart your membership</h3>
+                     <div className="lgnScreen__Input">
+                        <form>
+                            <input
+                            type="email"
+                            placeholder="Email Adress"
+                            />
+                            <button className="lgnScreen__getStartedBtn"
+
+                            >
+                                GET STARTED
+                            </button>
+                        </form>
+                     </div>
+                </>
+            </div>
         </div>
-  )
+    )
        
 }
 
